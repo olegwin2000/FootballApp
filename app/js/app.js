1e3d817d1f1beb6a449f353cfdfea5eb8805690f
Oleg Omelchenko <olegwin2000@gmail.com>
2015-12-23 18:08:20 +0200

    added app.js and route config

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,19 @@
-
+var footballApp = angular.module('footballApp', [
+    'ngRoute',
+    'footballAppControllers'
+]);
+footballApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/teams', {
+                templateUrl: 'partials/teams-list.html',
+                controller: 'TeamsListCtrl'
+            }).
+            when('/teams/:teamId', {
+                templateUrl: 'partials/team-info.html',
+                controller: 'TeamInfoCtrl'
+            }).
+            otherwise({
+                redirectTo: '/teams'
+            });
+}]);
