a4b85bbb7cd3c1e3615938c345edea9b4e5b6ec4
Oleg Omelchenko <olegwin2000@gmail.com>
2015-12-23 19:50:58 +0200

    Added Teams factory service

---
+++ app/js/services.js
@@ -0,0 +1,7 @@
+var footballAppServices = angular.module('footballAppServices', ['ngResource']);
+footballAppServices.factory('Team', ['$resource',
+    function($resource){
+        return $resource('/teams/:teamId.json', {}, {
+            query: {method: 'GET', params: {phoneId: 'phones'}, isArray:true}
+        });
+}]);
