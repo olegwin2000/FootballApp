a6b88087058a98e8713384f984eb4491e22ab467
Oleg Omelchenko <olegwin2000@gmail.com>
2015-12-26 13:46:12 +0200

    edited  chess-table template and directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,12 +3,7 @@ var footballAppDirectives = angular.module('footballAppDirectives', []);
 footballAppDirectives.directive('chessTable', function(){
     return {
         restrict: 'E',
-        template: '<span>tbd: chess-table</span>',
-        replace: true,
-        link: function($scope, element, attrs){
-            attrs.$observe('name', function(value){
-                element.find('span').text(value);
-            })
-        }
+        templateUrl: 'partials/chess-table.html',
+        replace: true
     }
 })
