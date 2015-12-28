7f91b90ce940ed9ce87043b2d23c1d8d729a1470
Oleg Omelchenko <olegwin2000@gmail.com>
2015-12-28 22:56:10 +0200

    improvements

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,5 +20,13 @@ footballAppControllers.controller('TeamInfoCtrl', ['$scope', '$routeParams', '$h
 footballAppControllers.controller('ChessTableCtrl', ['$scope', function($scope){
     $scope.data1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
     $scope.data2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-
+    $scope.data = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    $scope.changeValue = function(team1, team2, value){
+        $scope.data[team1][team2] = value;
+        $scope.data[team2][team1] = -value;
+    }
+    $scope.model = '1'
+    //$scope.log = function(){
+    //    console.log($scope.data);
+    //}
 }])
